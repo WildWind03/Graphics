a397287e08506ae5259b5bbfa759fbe8b0d07f31
WildWind03 <usergreat03@gmail.com>
2017-02-27 16:36:28 +0700

    Add upper folder

--- Life/src/main/java/Main.java
+++ Life/src/main/java/Main.java
@@ -6,9 +6,9 @@ import java.util.logging.Logger;
 public class Main {
     private static final Logger logger = Logger.getLogger(Main.class.getName());
 
-    private static final int lineLength = 30;
+    private static final int lineLength = 15;
 
     public static void main(String[] args) throws IOException {
-        GameController gameController = new GameController(20, 20, lineLength);
+        GameController gameController = new GameController(100, 100, lineLength);
     }
 }
