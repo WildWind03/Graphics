ed5537972bb44ac6e3f4e08923a94b1d4d3f3203
WildWind03 <usergreat03@gmail.com>
2017-03-19 17:06:25 +0700

    Delete Filter project

--- Filter/src/main/java/ru/nsu/fit/g14201/chirikhin/filter/main/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.nsu.fit.g14201.chirikhin.filter.main;
-
-import ru.nsu.fit.g14201.chirikhin.filter.controller.FilterController;
-
-public class Main {
-    public static void main(String[] args) {
-        FilterController filterController = new FilterController();
-    }
-}
--- Filter/src/main/java/ru/nsu/fit/g14201/chirikhin/filter/view/LoadImageException.java
+++
@@ -1,7 +0,0 @@
-package ru.nsu.fit.g14201.chirikhin.filter.view;
-
-public class LoadImageException extends Exception {
-    public LoadImageException(String message) {
-        super(message);
-    }
-}
